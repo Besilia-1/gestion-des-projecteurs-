7019561e2c8213d953cfb7f21db696440a598b85
Cybedril <cybedril.ags@gmail.com>
2025-03-01 08:35:19 +0100

    Protéger une route avec le middleware d’authentification

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const { registerUser, loginUser } = require('../controllers/userController');
+const authMiddleware = require("../middlewares/authMiddleware");
 
 const router = express.Router();
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
+// Route protégée : récupérer le profil utilisateur
+router.get("/profile", authMiddleware, (req, res) => {
+    res.json({ message: "Profil utilisateur récupéré avec succès", user: req.user });
+});
 
 module.exports = router;
